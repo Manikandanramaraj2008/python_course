ca44adb05d9186e9058d745f0aebe8d661878563
maniv <maniramkr2006@gmail.com>
2023-08-19 21:44:54 +0530

    python practices

---
+++ store.py
@@ -0,0 +1,155 @@
+# p = 300
+# q = 100
+# if p < q:
+#     print('p is greater than q')
+# else:
+#     print('q is greater than p')
+
+# def trialfunction():
+#     print("Hello world")
+# trialfunction()
+
+# def trialfunction(x):
+#     print(x+"world")
+# trialfunction("hello")
+
+# def trialfunction(x,y):
+#     print(x+" "+y)
+# trialfunction("hello", "world")
+
+# for loop
+
+# numbers = [1,2,3,10,20,30,50]
+# for x in numbers:
+#     print(x)
+
+# numbers =[1,2,3,4,10,20,30,40,100]
+# for x in numbers:
+#     print(x)
+#     if x==4:
+#      break
+
+# numbers =[1,2,3,4,10,20,30,40,100]
+# for x in numbers:
+#     print(x)
+#     if x==5:
+#      break
+
+# numbers =[1,2,3,4,10,20,30,40,100]
+# for x in numbers:
+#     if x==3:
+#      continue
+#     print(x)
+
+# numbers =[1,2,3,4,10,20,30,40,100]
+# for x in numbers:
+#     if x==5:
+#      continue
+#     print(x)
+
+
+# for x in range(100):
+#     print(x)
+
+# for x in range (50,100):
+#     print(x)
+
+# i=1
+# while i<=6:
+#     print(i)
+#     i+=1
+
+# i=1
+# while i<5:
+#     print(i)
+#     i+=2
+
+# i=1
+# while i<6:
+#     i+=1
+#     if i==3:
+#      continue
+#     print(i)
+
+# i = 1
+# while i < 6:
+#     i += 1
+#     if i == 5:
+#         break
+#     print(i)
+
+
+# i=0
+# while i<26:
+#     print(chr(97+i), end= " ")
+#     i=i+1
+
+# def printUppercase():
+#     i=0
+#     while i<26:
+#         print(chr(65+i), end=" ")
+#         i=i+1
+# printUppercase()
+
+
+# list =['car','bus','truck','scooter']
+# def loop(x):
+#     print(x*3)
+# loop(list)
+
+# list =['al','bl','cl','dl']
+# def loup(x):
+#     print(x*3)
+# def map_simple(crazy,list):
+#     for items in list:
+#         crazy(items)
+# map_simple(loup,list)
+
+#  list is orderable,modifiable,changeable, allowed duplicates
+
+list =['car',"par", 10,True,False,1.2]
+print(list)
+print(len(list))
+print(type(list))
+print(list[2])
+print(list[2:5])
+print(list[2:])
+print(list[:6])
+list[2]="scooter"
+print(list)
+list[2:4] =["bike","bmw","benz"]
+print(list)
+list.append("oranges")
+print(list)
+list.insert(7,"apple")
+print(list)
+
+
+
+
+
+
+
+#     ASCII (American Standard Code for Information Interchange)
+# unique values for 128 alphabetic, numeric or special additional characters and control codes.
+# The range of ASCII values for uppercase letters A-Z is 65-90, and the range for lowercase letters a-z is 97-122.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
