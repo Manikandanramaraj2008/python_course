b7384ba6e1a5de79779abac0c1f098698bea51dd
maniv <maniramkr2006@gmail.com>
2023-08-19 21:22:20 +0530

    python practices

---
+++ hello.py
@@ -0,0 +1,95 @@
+# print("Hello world")
+# # int
+#
+# x=4
+# print(type(x))
+# y=4.2
+# print(type(y))
+# z = 4j
+# print(type(z))
+#
+# a=float(x)
+# print(a)
+#
+# b=int(y)
+# print(b)
+#
+#
+# c=complex(x)
+# print(c)
+#
+# d= complex(y)
+# print(d)
+#
+# print(a/b)
+# print(a//b)
+# print(a*b)
+# print(a%b)
+#
+# if 5>3:
+#    print("Hello")
+
+
+
+
+
+
+# #***
+#
+#   the floor division  // returns the nearest whole integer point value.
+#
+#  **  exponential  f(x) = a(x)
+#
+#  modulus  % remainder operator.   get the remainder of division ,  modulus of 17/3 is 2
+# ********
+#  print "Hello"
+#   #a["3"]
+#
+# #KeyError: '3'
+# #>>> from math import squre
+#
+# ImportError: cannot import name 'squre' from 'math' (unknown location)
+# >>> it=iter([1,2,3])
+#
+# #>>> next(it)
+# #1
+# #>>> next(it)
+# 2
+# >>> next(it)
+# 3
+# >>> next(it)
+#
+# StopIteration
+# >>> "12"+2
+#
+# TypeError: can only concatenate str (not "int") to str
+#
+# >>> int("xy")
+#
+# ValueError: invalid literal for int() with base 10: 'xy'
+#
+#
+# >>> b
+#
+# NameError: name 'b' is not defined
+#
+# >>> 100/0
+#
+# ZeroDivisionError: division by zero
+#
+#
+# >>> name=input("enter your name")
+# enter your name
+# >
+# ********
+#
+#
+
+
+
+
+
+
+
+
+
